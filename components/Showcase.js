8207acecad3ede16581c19cc659af3eb03ec2531
mlazzarini <margherita.lazzarini@gmail.com>
2021-09-13 22:00:45 +0200

    Add static content

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+import Link from 'next/Link'
+
+export default function Showcase() {
+    return (
+        <div className={styles.showcase}>
+            <h1>Find the hottest DJ Events</h1>
+        </div>
+    )
+}
